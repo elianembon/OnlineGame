8354a926ecfe7d6bd8fb6de2f68fddb9ade97217
Owwly <luciachilievich@gmail.com>
2024-12-05 04:43:35 -0300

    Menus?

---
+++ Assets/Scripts/Menues/GoToMenu.cs
@@ -0,0 +1,14 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToMenu : MonoBehaviour
+{
+   
+    public void goToMenu()
+    {
+        PhotonNetwork.LoadLevel("Menu");
+    }
+
+}
