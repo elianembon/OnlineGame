8e449cc178273533229289826fe1835c04c3b191
Owwly <luciachilievich@gmail.com>
2024-12-05 01:54:59 -0300

    Funcionamiento correcto de las pantallas de carga

--- Assets/Scripts/Menues/ReconnectMenu.cs
+++ Assets/Scripts/Menues/ReconnectMenu.cs
@@ -13,61 +13,35 @@ public class ReconnectMenu : MonoBehaviourPunCallbacks
 
     void Start()
     {
+
+
+
         if (!PhotonNetwork.IsConnected)
         {
             PhotonNetwork.ConnectUsingSettings();
         }
+
         StartCoroutine(WaitBeforeStartGame());
-
     }
-
-    public void ShowPanelFailed()
-    {
-        reconnectPanel.SetActive(true);
-        connectPanel.SetActive(false);
-        StartCoroutine(WaitBeforeGoToMenu());
-
-    }
-
-
-    public override void OnDisconnected(DisconnectCause cause)
-    {
-        ShowPanelFailed();
-    }
-
-    /*public override void OnConnectedToMaster()
-    {
-        StartCoroutine(WaitBeforeStartGame());
-    }*/
 
     private IEnumerator WaitBeforeStartGame()
     {
-        Debug.Log("Esperando 5 segundos antes de entrar a la partida...");
-        yield return new WaitForSeconds(5f); // Espera 5 segundos
-        StartGame();
+        Debug.Log("Esperando 5 segundos antes de iniciar...");
+        yield return new WaitForSeconds(5f);
+        connectPanel.SetActive(false);
+        NotifySpawnPlayer(); // Notifica que el jugador puede ser instanciado
     }
 
-    private IEnumerator WaitBeforeGoToMenu()
+    private void NotifySpawnPlayer()
     {
-        Debug.Log("Esperando 5 segundos antes de entrar ir al menu...");
-        yield return new WaitForSeconds(5f); // Espera 5 segundos para volver al menu
-        PhotonNetwork.LoadLevel("Menu");
-    }
-
-
-    public void StartGame()
-    {
-        Debug.Log("Intentando iniciar el juego...");
-
-        if (PhotonNetwork.IsConnectedAndReady)
+        PlayerSpawn playerSpawn = FindObjectOfType<PlayerSpawn>();
+        if (playerSpawn != null)
         {
-            Debug.Log("Photon está conectado. Cargando la escena Gameplay...");
-            PhotonNetwork.LoadLevel("Gameplay");
+            playerSpawn.SpawnPlayer(); // Llama al método para instanciar al jugador
         }
         else
         {
-            Debug.LogError("Photon no está conectado o listo. No se puede cargar la escena Gameplay.");
+            Debug.LogError("No se encontró el script PlayerSpawn en la escena.");
         }
     }
-
 }
